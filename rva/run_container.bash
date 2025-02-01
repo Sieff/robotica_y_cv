8cdb37aabb8df1f26dfd06542d0d6f2160751d45
Steffen Schubert <s.schubert@folge3.de>
2025-02-01 20:16:35 +0100

    init

---
+++ rva/run_container.bash
@@ -0,0 +1,15 @@
+export DISPLAY=:0
+xhost +localhost
+
+docker run -it \
+    -e DISPLAY=host.docker.internal:0 \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
+    --net=host \
+    --privileged \
+    --name rva_container \
+    --mount type=bind,source=/Users/ste/Documents/Uni/robotica/rva/rva_exchange,target=/home/rva_container/rva_exchange \
+    rva_container \
+    bash
+    
+docker rm rva_container
